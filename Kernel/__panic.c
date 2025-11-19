29cb0f46712fb466873b29232cce87257dbbf7db
enzogabryelbezerra2018-ui <enzogabryelbezerra2018@gmail.com>
2025-11-19 00:25:37 -0300

    Create __panic.c

---
+++ Kernel/__panic.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void pear_panic() {
+printf("KERNEL PANIC!!!\n");
+printf("CODE ERROR: 0ffffff93949\n");
+printf("Error: PEAR_KERNEL_INCOMPATIBLE\n");
+printf("DISK1: UNKWONW error: INCOMPATIBLE_OS
+        DISK2: UNKWONW System: None
+        REGISTER: line 26: OS_NO_COMPATIBLE_ERROR[];
+  \n");
+}
