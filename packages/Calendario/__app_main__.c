d293d1bcfb12edfcc3628ab018a4e53dbaa0d055
enzogabryelbezerra2018-ui <enzogabryelbezerra2018@gmail.com>
2025-11-18 22:57:48 -0300

    Create __app_main__.c

---
+++ packages/Calendario/__app_main__.c
@@ -0,0 +1,7 @@
+#include <Car/x86/ui.c>
+UI_COLORS("black"); /** ativado no modo escuro */
+UI_COLORS("white"); /** modo normal */
+UI_TOUCH("TOUCH");
+UI_ALERT("bem-vindo ao calendário");
+UI_SCROLL("1 A.c and 2100");
+UI_DATABASE("https://localhost8080.com");
