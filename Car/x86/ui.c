4e3bab392a270fd2a2a4a24836225b05d12cbfdb
enzogabryelbezerra2018-ui <enzogabryelbezerra2018@gmail.com>
2025-11-18 22:44:02 -0300

    Create ui.c

---
+++ Car/x86/ui.c
@@ -0,0 +1,70 @@
+/**
+copyright (C) pearOS - enzo
+*/
+MODULE_LICENSE("GPL/GNU");
+MODULE_AUTHOR("Enzo gabryel");
+MODULE_DESCRIPTION("Ui do PearCar");
+#define UI_BUTTON
+#define UI_TEXT
+#define UI_LABEL_TEXT
+#define UI_SIZE_DP
+#define UI_COLORS
+#define UI_RGB
+#define UI_R
+#define UI_G
+#define UI_B
+#define UI_SRGB
+#define UI_S
+#define UI_R
+#define UI_G
+#define UI_B
+#define UI_PEARCAR_WAIT_AUTOCAR_USB_ARM64
+#define UI_COLOR_RED
+#define UI_COLOR_GREEN
+#define UI_COLOR_BLUE
+#define UI_ONCLICK_BUTTON
+#define UI_TALKBACK
+#define UI_DOCK
+#define UI_CAR_SET_SUN_64
+#define UI_X86
+#define UI_X86_64
+#define UI_ARM
+#define UI_ARM64
+#define UI_SCROLL
+#define UI_Y
+#define UI_X
+#define UI_Z
+#define UI_LUA_ENGINE
+#define UI_LUA_PEAR_LIBS
+#define UI_ALERT_NORMAL
+#define UI_ALERT_MEDIUM
+#define UI_ALERT_EMERGENCE
+#define UI_NOTFICATION_INTERFACE
+#define UI_DATABASE
+#define UI_ART
+#define UI_CAR_ARCH
+#define UI_AUDIO
+#define UI_WIFI
+#define UI_SPEAKER_CONTROLER /** controlador do alto falante */
+#define UI_LOANDING_BAR_FRAME_1
+#define UI_LOANDING_BAR_FRAME_2
+#define UI_LOANDING_BAR_FRAME_3
+#define UI_LOANDING_BAR_FRAME_4
+#define UI_LOANDING_BAR_FRAME_5
+#define UI_LOANDING_BAR_FRAME_6
+#define UI_LOANDING_BAR_FRAME_7
+#define UI_LOANDING_BAR_FRAME_8
+#define UI_LOANDING_BAR_FRAME_9
+#define UI_LOANDING_BAR_FRAME_10
+#define UI_LOANDING_BAR_FRAME_11
+#define UI_LOANDING_BAR_FRAME_12
+#define UI_LOANDING_BAR_FRAME_13
+#define UI_LOANDING_BAR_FRAME_14
+#define UI_APPS
+#define UI_TOUCH
+#define UI_MOUSE
+#define UI_LOAD_UI
+#define UI_COLORE_CODES
+printk("Ui do car/PC/celular foi carregada com sucesso");
+printk("PearOS iniciando...");
+system("clear");
